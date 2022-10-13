c92c6bf4e7c00132c4089112afeeb0e2fe06754f
montage0921 <garyshi0921@gmail.com>
2022-10-13 14:14:55 -0400

    write playRound()

--- javascript.js
+++ javascript.js
@@ -2,7 +2,38 @@ const rockPaperScissor = [`rock`, `paper`, `scissor`];
 
 function getComputerChoice() {
   const i = Math.floor(Math.random() * 3);
-  console.log(rockPaperScissor[i]);
+  return rockPaperScissor[i];
 }
 
-getComputerChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  //guard clause
+  if (
+    playerSelection !== `rock` &&
+    playerSelection !== `paper` &&
+    playerSelection !== `scissor`
+  )
+    return;
+
+  console.log(playerSelection, computerSelection);
+
+  if (playerSelection === computerSelection) console.log(`Tie Game 🟰`);
+  else if (playerSelection === `rock`) {
+    return computerSelection === `paper`
+      ? `You Lose! Paper beats Rock 🖐`
+      : `You Win! Rock beats Scissor 👊🏻`;
+  } else if (playerSelection === `paper`) {
+    return computerSelection === `rock`
+      ? `You Win! Paper beats Rock 🖐`
+      : `You Lose! Scissor beats paper ✌🏿`;
+  } else if (playerSelection === `scissor`) {
+    return computerSelection === `rock`
+      ? `You Lose! Rock beats Scissor 👊🏻 `
+      : `You Win! Scissor beats paper ✌🏿`;
+  }
+}
+
+const playerSelection = prompt(`Enter rock, paper or scissor!`).toLowerCase();
+
+console.log(playRound(playerSelection, computerSelection));
