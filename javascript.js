6c1c9f46357214011c2fd29a00b952482cd90bce
montage0921 <garyshi0921@gmail.com>
2022-10-13 13:51:05 -0400

    Write getComputer Chocice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-console.log(`Hello`);
+const rockPaperScissor = [`rock`, `paper`, `scissor`];
+
+function getComputerChoice() {
+  const i = Math.floor(Math.random() * 3);
+  console.log(rockPaperScissor[i]);
+}
+
+getComputerChoice();
